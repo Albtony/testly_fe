0d9511e249bb30ee73a5937a1cd6947b346c5561
Nuzul Abatony <albtony21@gmail.com>
2025-05-12 01:32:19 +0700

    feat: improve app component structure with semantic HTML and CSS classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HelloPage from './pages/HelloPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <nav>
+            <ul className="App-nav">
+              <li className="App-nav-item">
+                <Link to="/hello-fe" className="App-link">
+                  Hello Backend
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <main className="App-main">
+          <div className="page-content">
+            <Routes>
+              <Route path="/hello-fe" element={<HelloPage />} /> {/* Define the route for HelloPage */}
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
